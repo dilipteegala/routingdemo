f20b5360de2f5d179ca2a2792f44cd1b89efd822
dilipteegala <diliprao0007@gmail.com>
2019-02-03 12:35:36 +0530

    data binding in forms

---
+++ src/app/employee.model.ts
@@ -0,0 +1,10 @@
+export class Employee {
+
+    constructor(
+      public  firstname: string,
+      public  lastname: string,
+      public  isFullTime:boolean,
+      public paytype:string
+      ) {  }
+  
+  }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Employee }    from '../employee.model';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  
+  model = new Employee("dilip","rao",true,"");
   lang:['English','spanish','Other'];
   constructor() { }
 
